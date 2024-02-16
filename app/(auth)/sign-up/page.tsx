6c8945d20b0cc376d66b3a213e263fcf539e682c
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-16 08:00:39 -0300

    feat: add redirect user with logged

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
 import Link from "next/link";
-import GoogleIcon from '@/public/google.svg';
-import Image from "next/image";
+import GithubSignInButton from "@/app/components/GithubSignInButton";
+import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/auth";
+import { redirect } from "next/navigation";
 
-export default function SignUp(){
+export default async function SignUp(){
+    const session = await getServerSession(authOptions);
+
+    if(session){
+        return redirect("/home");
+    }
+
     return (
         <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form>
@@ -33,12 +41,8 @@ export default function SignUp(){
             </div>
 
             <div className="flex w-full justify-center items-center gap-x-3 mt-6">
-                <Button variant="outline" size="icon">
-                    <GithubIcon className="w-4 h-4"/>
-                </Button>
-                <Button variant="outline" size="icon">
-                    <Image src={GoogleIcon} alt="Google" className="w-6 h-6" />
-                </Button>
+                <GithubSignInButton />
+                <GoogleSignInButton />
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./utils/auth";
+import { redirect } from "next/navigation";
 
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
-export default function Home() {
-  return (
-    <div>
-      <Button>Hellow World</Button>
-    </div>
-  );
+  if(!session){
+    return redirect('/login');
+  } else {
+    return redirect('/home')
+  }
 }
